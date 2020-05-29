764054ddd36ba254329c677b202b1e63ccfbfba4
HookedBehemoth <Luisscheurenbrand@gmail.com>
2020-05-29 21:08:37 +0200

    re-add pl update

--- libs/include/tesla.hpp
+++ libs/include/tesla.hpp
@@ -3612,7 +3612,7 @@ extern "C" {
         tsl::hlp::doWithSmSession([]{
             ASSERT_FATAL(fsInitialize());
             ASSERT_FATAL(hidInitialize());                          // Controller inputs and Touch
-            ASSERT_FATAL(plInitialize());                           // Font data. Use pl:s to prevent qlaunch/overlaydisp session exhaustion
+            ASSERT_FATAL(plInitialize(PlServiceType_System));       // Font data. Use pl:s to prevent qlaunch/overlaydisp session exhaustion
             ASSERT_FATAL(pmdmntInitialize());                       // PID querying
             ASSERT_FATAL(hidsysInitialize());                       // Focus control
             ASSERT_FATAL(setsysInitialize());                       // Settings querying
